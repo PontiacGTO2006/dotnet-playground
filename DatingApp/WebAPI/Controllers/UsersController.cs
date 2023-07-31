46e29791ba8eec94b6fb600ed856eeca05c50964
PontiacGTO2006 <101056722+PontiacGTO2006@users.noreply.github.com>
2023-07-31 18:02:19 -0500

    Entity Framework and SQLite additions
    Integrated EF and portable SQLite database features (although it's still a bit flaky.) Still working on setting up an API controller.

---
+++ DatingApp/WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+using WebAPI.Controllers
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // /WebApi/users
+    public class UsersController
+    {
+        
+    }
+}
---
+++ DatingApp/WebAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using WebAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
