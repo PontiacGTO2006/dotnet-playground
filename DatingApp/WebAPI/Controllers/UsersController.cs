091ba2ad4b3119385f93ec953e165fd7d6aea26c
PontiacGTO2006 <101056722+PontiacGTO2006@users.noreply.github.com>
2023-08-02 18:01:49 -0500

    Error codes go bah
    Yeah, pretty bad

--- DatingApp/WebAPI/Controllers/UsersController.cs
+++ DatingApp/WebAPI/Controllers/UsersController.cs
@@ -1,12 +1,30 @@
-using Microsoft.AspNetCore.Components;
-using WebAPI.Controllers
+using WebAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Entities;
 
 namespace WebAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")] // /WebApi/users
-    public class UsersController
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")] // /WebApi/users
+    public class UsersController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
     }
 }
