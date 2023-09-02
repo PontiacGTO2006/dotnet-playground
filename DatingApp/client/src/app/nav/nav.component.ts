4dfbe047dc38f261aaa36dd193153088b8dbc726
PontiacGTO2006 <101056722+PontiacGTO2006@users.noreply.github.com>
2023-09-02 11:07:33 -0500

    Added observables and persisted login
    Added login/logout functionality in the browser and a login dropdown menu, in addition to persisting the login function and storing user data.

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   model: any = {}
+  loggedIn = false;
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
-  login () {
-    console.log(this.model);
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe({
+      next: user => this.loggedIn = !!user, 
+      error: error => console.log(error)
+    })
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.loggedIn = false;
   }
 }
